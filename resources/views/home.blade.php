077301e4fd4ec43eb7f74d5bf5f1fd13b932a39a
小林加弥 <2021php41-student029@la-study.com>
2021-05-20 14:13:07 +0900

    add user_search

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,8 @@
 
 @section( 'tbody' )
 <p uk-margin>
-    <button class="uk-button uk-button-default uk-button-large">会員管理</button>
+    <button type="button" class="uk-button uk-button-default uk-button-large" onclick="windows.location='{{ route( 'user_search' ) }}'">会員管理</button>
+    <button type="button" onclick="window.location='{{ route("user_search") }}'">Button</button>
     <button class="uk-button uk-button-default uk-button-large">宿管理</button>
 </p>
 @endsection
