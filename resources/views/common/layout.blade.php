c872682af5dea1aec01e88c1bbdaef3741ac127d
小林加弥 <2021php41-student029@la-study.com>
2021-05-21 15:35:13 +0900

    delete uikit class

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -5,9 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config( 'app.name' ) }}</title>
-    
+    <!-- CSS -->
     <link rel="stylesheet" href="{{ asset('/css/layout.css') }}">
-
 </head>
 <body>
     <div> 
