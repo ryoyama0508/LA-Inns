5f73faa0d288e1f832122a97cf8429fc9fd1fab7
小林加弥 <2021php41-student029@la-study.com>
2021-05-21 13:41:54 +0900

    disconnect uikit links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,17 @@
 
 @section( 'header' )
     <div >
-        <a href="">戻る</a>
+        <a href="" id="backhome">戻る</a>
     </div>
     <div>
-        <h2>ホーム</h2>
+        <h2 id="home">ホーム</h2>
     </div>
 @endsection
 <hr>
 
 @section( 'tbody' )
 <div>
-    <a  type="button" href="{{ route( 'users.index' ) }}">会員検索</a>
-    <a  type="button" href="{{ route( 'inns.index' ) }}">宿管理</a>
+    <a id="user_serch" type="button" href="{{ route( 'users.index' ) }}">会員検索</a>
+    <a id=innmg type="button" href="{{ route( 'inns.index' ) }}">宿管理</a>
     </div>
 @endsection
